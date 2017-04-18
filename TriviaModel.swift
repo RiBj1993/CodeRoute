f92a6c6815d8b8b66232a9381210f3f3da65706d
SwiftR <swiftr@mac-mini-de-esprit-2.local>
2017-04-18 05:43:45 +0100

    pppp

--- TriviaModel.swift
+++ TriviaModel.swift
@@ -9,6 +9,9 @@
 
 // declarring a class containing the data set for the trivia questions and answers
 // thirdChoice and fourthChoice are optionals as they will be part of True/False questions
+import FirebaseDatabase
+import FirebaseStorage
+import Firebase
 
 class Trivia {
     
@@ -18,7 +21,9 @@ class Trivia {
     let thirdChoice: String
     let fourthChoice: String
     let correctAnswer: String
-    
+   
+   
+  
     init(question: String, firstChoice: String, secondChoice: String, thirdChoice: String, fourthChoice: String, correctAnswer: String) {
         self.question = question
         self.firstChoice = firstChoice
@@ -27,9 +32,110 @@ class Trivia {
         self.fourthChoice = fourthChoice
         self.correctAnswer = correctAnswer
         
-    }
+      bbbbbbc()
+      
+        }
+        
+   
+   
 }
 
+
+
+func  bbbbbbc () {
+    
+    let ref: FIRDatabaseReference!
+    ref = FIRDatabase.database().reference()
+    //var i : Int = 1
+    var stringValue: String = "1"
+    
+    var  count: Int = 0
+     let nbDeLignes: Int = 0
+    
+    print(nbDeLignes)
+    
+    ref.child("qwiz").observeSingleEvent(of: .value, with: { (snapshot) in
+        
+        if !( snapshot.value  is NSNull){
+         
+            count = Int(snapshot.childrenCount )
+              print("count",count)
+            for nbDeLignes in 0...(count+1) {
+                print("\(nbDeLignes). Je dois apprendre mes leçons en cours de Swift.")
+                butto(VV:stringValue)
+                count += 1
+                stringValue = "\(nbDeLignes)"
+               print("stringValue",stringValue)
+            }
+
+        }
+    }) { (error) in
+        print(error.localizedDescription)
+    }
+    
+   
+    
+    
+    
+   
+
+}
+
+func butto(VV:String) {
+    
+    let ref: FIRDatabaseReference!
+    
+    ref = FIRDatabase.database().reference()
+    
+    // Use you snapshot(FIRDataSnapshot) to create the data of the room.
+   var  TriviaList : Array<Trivia> = Array()
+    
+    ref.child("qwiz").child(VV).observeSingleEvent(of: .value, with: { (snapshot) in
+        // Get user value
+        
+       if !( snapshot.value  is NSNull){
+            
+            
+            
+            let value = snapshot.value as? NSDictionary
+            let choix1 = value?["choix1"] as? String ?? ""
+            let choix2 = value?["choix2"] as? String ?? ""
+            let choix3 = value?["choix3"] as? String ?? ""
+            let choix4 = value?["choix4"] as? String ?? ""
+            let Breponse = value?["reponse"] as? String ?? ""
+            let IMAGE = value?["image"] as? String ?? ""
+            print(choix1)
+            print(choix2)
+            print(choix3)
+            print(choix4 )
+            print(IMAGE)
+            print(Breponse)
+            print("ppppppppppppppp",snapshot.childrenCount)
+        let room = Trivia(question: choix1,
+                          firstChoice: choix2,
+                          secondChoice: choix3,
+                          thirdChoice: Breponse,
+                          fourthChoice: IMAGE,
+                          correctAnswer: Breponse)
+        
+
+     
+        
+    
+
+        TriviaList.append(room)
+        
+        var i = 0
+        while i < TriviaList.count
+        {
+            olympicTriviaQuestions[i] = TriviaList.index(i, offsetBy: 1)
+            i += 1
+        }
+        }
+    }) { (error) in
+        print(error.localizedDescription)
+    }
+}
 // trivia questions with answer choices and correctAnswer declared
 
 let triviaQuestion01 = Trivia(question: "In which city were the first Modern Olympics held",
@@ -116,7 +222,7 @@ let triviaQuestion12 = Trivia(question: "When was the first Olympic torch relay 
                               fourthChoice: "1936",
                               correctAnswer: "1936")
 
-let olympicTriviaQuestions = [triviaQuestion01,
+var olympicTriviaQuestions = [triviaQuestion01,
                               triviaQuestion02,
                               triviaQuestion03,
                               triviaQuestion04,
@@ -129,3 +235,5 @@ let olympicTriviaQuestions = [triviaQuestion01,
                               triviaQuestion11,
                               triviaQuestion12
 ]
+
+
